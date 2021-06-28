efea31b6b40a23b4b4624de88fdecc699d1d390d
Claudio Pereira <claudiofrancopereira@gmail.com>
2021-06-27 21:01:36 -0300

    Tag Creation

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -1,10 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm'
-
 import { User } from '../models/User';
 
 @EntityRepository(User)
-class UsersRepositories extends Repository<User>{
-
-}
+class UsersRepositories extends Repository<User>{}
 
 export { UsersRepositories }
---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,29 @@
+import { getCustomRepository } from "typeorm";
+import { TagsRepositories } from "../repositories/TagsRepositories";
+
+class CreateTagService {
+    async execute(name: string) {
+        const tagsRepositories = getCustomRepository(TagsRepositories)
+
+        if(!name) {
+            throw new Error('Invalid name!');
+        }
+
+        const tagAlreadyExistis = await tagsRepositories.findOne({
+            name
+        });
+
+        if(tagAlreadyExistis) {
+            throw new Error('Tag already exists');
+        }
+
+        const tag = tagsRepositories.create({
+            name
+        });
+
+        await tagsRepositories.save(tag);
+        return tag;
+    }
+}
+
+export { CreateTagService }
