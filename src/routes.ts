0b093b2758992980f895b7b20c37a39865960d49
Claudio Pereira <claudiofrancopereira@gmail.com>
2021-07-03 15:32:18 -0300

    Added @types/express for user_id Compliment finished

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from './controllers/CreateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
+
 const router = Router();
 
 const createUserController = new CreateUserController();
@@ -12,8 +14,8 @@ const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
 
 router.post('/users', createUserController.handle);
-router.post('/tags', ensureAdmin, createTagController.handle);
+router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post('/login', authenticateUserController.handle);
-router.post('/compliments', createComplimentController.handle);
+router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
 
 export { router }
